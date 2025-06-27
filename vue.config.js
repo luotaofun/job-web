fc0f1dc5d518791590e88a9a6debef455fcd93e5
luotaofun <luotao0213@163.com>
2025-06-27 13:16:42 +0800

    2025年6月27日13:16:41

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
     devServer:{
         proxy:{
             '/api':{
-                target:'http://127.0.0.1:8001/',
+                target:'http://127.0.0.1:8081/',
                 pathRewrite:{
                     '^/api':''
                 },
